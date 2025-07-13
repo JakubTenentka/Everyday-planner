a0d5e0694b07f23ee5f1cd194d18d0aa34cf6a67
JakubTenentka <247812@edu.p.lodz.pl>
2025-07-13 13:56:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# To Do App
+
+A full-stack to-do application using:
+
+- 📱 **Frontend**: Flutter (in `To_do_Flutter/`)
+- 🌐 **Backend**: Spring Boot (in `To_do_Java/`)
+
+
+
---
+++ To_do_Java/src/test/java/org/project/to_do_java/ToDoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.to_do_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
